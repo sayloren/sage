2b1faa4927a21ba0e56e846df8c11613952edd7c
Wren <sayloren@gmail.com>
2016-12-14 13:22:31 -0500

    Streamlined DensityConverter

--- DensityConverter.py
+++ DensityConverter.py
@@ -11,6 +11,8 @@ maker
 
 Version 2: added sort
 
+Version 3: streamlined
+
 Wren Saylor, adapted from Ruth's Density Script
 December 13 2016
 
@@ -40,46 +42,39 @@ def getFeatures(strFileName):
 	preSortbtFeatures = pbt.BedTool(strFileName)
 	btFeatures = preSortbtFeatures.sort()
 	return btFeatures
-	
+
 # 3 - left outer join intersect 
 def intersectWindow(btWindows, btFeatures):
 	btIntersect = btWindows.intersect(btFeatures,loj=True)
 	return btIntersect
 
-# 4 - return windows with attached densities
+# 4 - grab just columns for window coordinates and densities
 def cutToFraction(btIntersect):
 	btCutToFraction = btIntersect.cut([0,1,2,6])
 	return btCutToFraction
 
-# 5 - perform the 3 and 4
-def getDensity(btWindows, btFeatures):
-	btIntersect = intersectWindow(btWindows, btFeatures)
-	btCutToFraction = cutToFraction(btIntersect)
-	return btCutToFraction
-
-# 6 - intersect, then merge the features in the window
+# 5 - merge the density features in the windows by mean
 def mergeWindow(btCutToFraction):
+	print btCutToFraction
 	btMerge = btCutToFraction.merge(c=4,o="mean")
 	return btMerge
 
-# 7 - save to bedtool
+# 6 - save to bedtool
 def saveBedTool(btObject, strFilename):
 	btObject.saveas(strFilename)
-
-# 8 - string commands together
-def forEachFeatureFile(strFileName, btWindows):
-	btFeatures = getFeatures(strFileName)
-	btCutToFraction = getDensity(btWindows, btFeatures)
-	btMerge = mergeWindow(btCutToFraction)
-	saveBedTool(btMerge, str('Density_for_matrix_{0}.bed'.format(strFileName)))
-	return btMerge
 
 def main():
 	args = get_args()
 	btWindows = getFeatures(args.bin)
 	arFilenames = getFileNames(args)
 	for filename in arFilenames:
-		btMerge = forEachFeatureFile(filename, btWindows) #, btCutToFraction
+		btFeatures = getFeatures(filename)
+		
+		#print btFeatures # doesn't print - probably a mal-formed row
+		btIntersect = intersectWindow(btWindows,btFeatures)
+		btCutToFraction = cutToFraction(btIntersect)
+		btMerge = mergeWindow(btCutToFraction)
+		saveBedTool(btMerge, str('Density_for_matrix_{0}.bed'.format(filename)))
 
 if __name__ == "__main__":
 	main()
