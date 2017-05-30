2023d0d7e25ee7577712326f6cdb6312fdb1802b
Wren <sayloren@gmail.com>
2017-05-30 17:17:26 -0400

    merge correctly in DensityConverter, instead of over merging

--- DensityConverter.py
+++ DensityConverter.py
@@ -15,6 +15,8 @@
     
     Version 3: streamlined function
     
+    Version 4: used alternative to bedtools merge for combining on density, to avoid over merging
+    
     Wren Saylor, adapted from Ruth's Density Script
     December 13 2016
     
@@ -57,14 +59,31 @@ def cutToFraction(btIntersect):
     btCutToFraction = btIntersect.cut([0,1,2,6])
         return btCutToFraction
 
-# 5 - merge the density features in the windows by mean
+# 5 - bedtool to panda for merge to be implemented correctly
+def bedtoolToPanda(btobject):
+    pdObject = pd.read_table(btobject.fn, header=None)
+        return pdObject
+
+# 5i - merge the density features in the windows by mean - depreciated; will merge more extensively than desired
 def mergeWindow(btCutToFraction,mode):
     btMerge = btCutToFraction.merge(c=4,o=mode)
         return btMerge
 
-# 6 - save to bedtool
+# 6 - merge density features
+def mergeWindow(pdFraction,mode):
+    pdFraction.columns = ["chr","start","stop","den"]
+        pdFraction.den[pdFraction.den == '.'] = 0
+        pdFraction[["den"]] = pdFraction[["den"]].astype(float)
+        pdGroup = pdFraction.groupby(["chr","start","stop"],as_index=False)["den"].mean()
+        return pdGroup
+
+# 7i - save to bedtool - depreciated; no longer output from bedtool
 def saveBedTool(btObject, strFilename):
     btObject.saveas(strFilename)
+
+# 7 - save file from panda
+def savePanda(pdData, strFilename):
+    pdData.to_csv(strFilename, sep='\t', header=False, index=True)
 
 def main():
     args = get_args()
@@ -74,8 +93,9 @@ def main():
             btFeatures = getFeatures(filename)
                 btIntersect = intersectWindow(btWindows,btFeatures)
                 btCutToFraction = cutToFraction(btIntersect)
-                btMerge = mergeWindow(btCutToFraction,args.mode)
-                saveBedTool(btMerge, str('Density_for_matrix_{0}.bed'.format(filename)))
+                pdFraction = bedtoolToPanda(btCutToFraction)
+                btMerge = mergeWindow(pdFraction,args.mode)
+                savePanda(btMerge, str('Density_for_matrix_{0}.bed'.format(filename)))
 
 if __name__ == "__main__":
     main()
