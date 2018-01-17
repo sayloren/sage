61622967261d69595b031dfa507b90da9a5704fc
Wren <sayloren@gmail.com>
2018-01-17 17:12:54 -0500

    getting some stats working for nearbyfeatures

--- nearbyfeatures.py
+++ nearbyfeatures.py
@@ -10,6 +10,20 @@ To Do:
 -Stats per overlaps (min/max, average, std) 
 -Where do overlaps occur
 
+Copyright 2018 Harvard University, Wu Lab
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 """
 import argparse
 import pandas as pd
@@ -17,10 +31,10 @@ import pybedtools as pbt
 
 def get_args():
 	parser = argparse.ArgumentParser(description="Description")
-	parser.add_argument("file", type=str,help='the primary element file') # UCEs
+	parser.add_argument("file",type=str,help='the primary element file') # UCEs
 	parser.add_argument("-s","--secondaryfeatures",type=argparse.FileType('rU'),help="a file with a list of file names with the secondary features to query") # Domains
 	parser.add_argument("-t","--tertiaryfeatures",type=argparse.FileType('rU'),help="a file with a list of file names with the tertiary features to query")# Genes
-	parset.add_argument("-g","--genome",type=argparse.FileType('rU'),help="genome file",default='hg19.genome')
+	parser.add_argument("-g","--genomefile",type=str,help="genome file",default='hg19.genome')
 	return parser.parse_args()
 
 # get bt features
@@ -30,10 +44,13 @@ def get_bedtools_features(strFileName):
 
 # get stats
 def descriptivestats(btfeature,genomefile):
-	genomecoverage = btfeature.genome_coverage(g=genomefile) # genome coverage
+# 	coverage = btfeature.genome_coverage(genome=genomefile) # genome coverage, not working yet
 	totalcount = btfeature.count() # total number of features in bedfile
-	# size (ave, std, range)
+	
+	
+	
 	# distance to other features in group
+	# size (ave, std, range) - as pd
 	# get relevant info into pd
 	return pdstats
 
@@ -42,19 +59,22 @@ def descriptivestats(btfeature,genomefile):
 def main():
 	args = get_args()
 	
-	# 1) read in
+	# 1) read in files from args
 	primaryfile = args.file
 	secondaryfiles = [line.strip() for line in args.secondaryfeatures]
 	tertiaryfiles = [line.strip() for line in args.tertiaryfeatures]
 	
-	genome = args.genome
+	genomefile = args.genomefile
 	
-	allfiles = primaryfile + secondaryfiles + tertiaryfiles
+	allfiles = secondaryfiles + tertiaryfiles
+	allfiles.insert(0,primaryfile)
+	
+	print 'running script for {0}'.format(allfiles)
 	
 	# 2) descriptive stats per feature
 	for file in allfiles:
-		btfeature = get_bedtools_features(file)
-		btstats = descriptivestats(btfeature,genome)
+		btfeature = get_bedtools_features(file) # get bed file features
+		btstats = descriptivestats(btfeature,genomefile) # get some stats about each file
 		# collect stats into df
 		# save stats file
 	
