ad7129be80e685342db2338776eb2531a5a1631f
Wren <sayloren@gmail.com>
2018-01-17 16:37:44 -0500

    read in files on nearbyfeatures.py

--- nearbyfeatures.py
+++ nearbyfeatures.py
@@ -5,11 +5,6 @@ Script to look at how close features are between two bed files, or a UCE file an
 Wren Saylor
 Jan 2018
 
-Files to run:
--UCEs
--Domains (not pooled)
--Genes
-
 To Do:
 -Stats per bed file features (number, size, genome coverage)
 -Stats per overlaps (min/max, average, std) 
@@ -22,16 +17,50 @@ import pybedtools as pbt
 
 def get_args():
 	parser = argparse.ArgumentParser(description="Description")
-	parser.add_argument("file", type=str,help='the primary element file')
-	parser.add_argument("-s","--secondaryfeatures",type=argparse.FileType('rU'),help="a list of files with the secondary features to query")
-	parser.add_argument("-t","--tertiaryfeatures",type=argparse.FileType('rU'),help="a list of files with the tertiary features to query")
+	parser.add_argument("file", type=str,help='the primary element file') # UCEs
+	parser.add_argument("-s","--secondaryfeatures",type=argparse.FileType('rU'),help="a file with a list of file names with the secondary features to query") # Domains
+	parser.add_argument("-t","--tertiaryfeatures",type=argparse.FileType('rU'),help="a file with a list of file names with the tertiary features to query")# Genes
+	parset.add_argument("-g","--genome",type=argparse.FileType('rU'),help="genome file",default='hg19.genome')
 	return parser.parse_args()
 
+# get bt features
+def get_bedtools_features(strFileName):
+	btFeatures = pbt.BedTool(strFileName)
+	return btFeatures
+
+# get stats
+def descriptivestats(btfeature,genomefile):
+	genomecoverage = btfeature.genome_coverage(g=genomefile) # genome coverage
+	totalcount = btfeature.count() # total number of features in bedfile
+	# size (ave, std, range)
+	# distance to other features in group
+	# get relevant info into pd
+	return pdstats
 
 
 
 def main():
-
+	args = get_args()
+	
+	# 1) read in
+	primaryfile = args.file
+	secondaryfiles = [line.strip() for line in args.secondaryfeatures]
+	tertiaryfiles = [line.strip() for line in args.tertiaryfeatures]
+	
+	genome = args.genome
+	
+	allfiles = primaryfile + secondaryfiles + tertiaryfiles
+	
+	# 2) descriptive stats per feature
+	for file in allfiles:
+		btfeature = get_bedtools_features(file)
+		btstats = descriptivestats(btfeature,genome)
+		# collect stats into df
+		# save stats file
+	
+	
+	# 3) get overlaps - secondary; intersect
+	# 4) get nearest - tertiary; closest
 
 
 
