9893318d584b491aa16daa6cb7e9bbcea510ce76
Wren <sayloren@gmail.com>
2018-01-19 17:14:44 -0500

    basic structure laid out for each of the three editor questions for ruth and jelena's reviews

--- nearbyfeatures.py
+++ nearbyfeatures.py
@@ -39,15 +39,13 @@ def get_args():
 
 # get bt features
 def get_bedtools_features(strFileName):
-	btFeatures = pbt.BedTool(strFileName)
-	return btFeatures
+	return pbt.BedTool(strFileName)
 
 # convert bedtool to panda
 def convert_bedtools_to_panda(btfeature):
-	pdfeature = pd.read_table(btfeature.fn,header=None)
-	return pdfeature
+	return pd.read_table(btfeature.fn,header=None)
 
-# label chr start and stop, make size
+# 4 cols coord labels
 def label_coordinate_columns(pdfeature):
 	pdfeature['size'] = pdfeature.loc[:,2].astype(int)-pdfeature.loc[:,1].astype(int)
 	pdfeature.columns.values[0]='chr'
@@ -56,9 +54,8 @@ def label_coordinate_columns(pdfeature):
 	return pdfeature
 
 # get stats
-def descriptive_stats(pdfeature,column):
-	pdstat = pdfeature[column].describe()
-	return pdstat
+def panda_describe_column(pdfeature,column):
+	return pdfeature[column].describe()
 
 # save panda
 def save_panda(pdData,strFilename):
@@ -66,51 +63,86 @@ def save_panda(pdData,strFilename):
 
 # intersect a file by how many times a feature on b is in the interval
 def intersect_bedfiles_c_true(afile,bfile):
-	intersect = afile.intersect(bfile,c=True)
-	return intersect
+	return afile.intersect(bfile,c=True)
 
+# total number of elements without overlaps
 def count_number_with_zero_overlaps(df,column):
-	print len(df[(df[column]==0)])
+	return len(df[(df[column]==0)])
 
 # move elements without any overlaps
 def remove_rows_with_no_overlaps(overlaps,column):
-	pdfeature = overlaps[overlaps[column]!=0]
+	return overlaps[overlaps[column]!=0]
+
+# Query: Return some info about the element size stats from file
+def descriptive_stats(file):
+	btfeature = get_bedtools_features(file)
+	pdfeature = convert_bedtools_to_panda(btfeature)
+	pdcoord = label_coordinate_columns(pdfeature)
+	pdstats = panda_describe_column(pdcoord,'size')
+	print 'feature stats on element sizes for {0}'.format(file)
+# 	print pdstats
+	return pdstats
+
+# Query: How many UCEs are there in a domain
+def overlaping_features(primary,sfile):
+	secondary = get_bedtools_features(sfile)
+	intersect = intersect_bedfiles_c_true(secondary,primary)
+	pdfeature = convert_bedtools_to_panda(intersect)
+	pdcoord = label_coordinate_columns(pdfeature)
+	pdcoord.columns.values[3]='intersect'
+	nooverlaps = count_number_with_zero_overlaps(pdcoord,'intersect')
+	print '{0} instances of no overlaps on {1}'.format(nooverlaps,sfile)
+	pdclean = remove_rows_with_no_overlaps(pdcoord,'intersect')
+	return secondary,pdclean
+
+# intersect files and get original coords and overlap size
+def intersect_bedfiles_wo_true(afile,bfile):
+	return afile.intersect(bfile,wo=True)
+
+# 7 cols coord labels
+def label_expanded_coordinate_columns(pdfeature):
+	pdfeature.columns.values[0]='achr'
+	pdfeature.columns.values[1]='astart'
+	pdfeature.columns.values[2]='aend'
+	pdfeature.columns.values[3]='bchr'
+	pdfeature.columns.values[4]='bstart'
+	pdfeature.columns.values[5]='bend'
+	pdfeature.columns.values[6]='overlapsize'
 	return pdfeature
 
+# make columns for distance between element and each boundary
+def get_boundary_distances(pdfeature):
+	pdfeature['enddistance'] = pdfeature['bend']-pdfeature['aend']
+	pdfeature['startdistance'] = pdfeature['bstart']-pdfeature['astart']
+	return pdfeature
 
+# determine which boundary distance is further
+def binary_boundary_distances(pdfeature):
+	pdfeature['closerboundary'] = np.where((pdfeature['startdistance'] >= pdfeature['enddistance']),'start','end')
+	return pdfeature
 
+# Query: Where in the domain are the UCEs
+def locateing_features(primary,file):
+	intersect = intersect_bedfiles_wo_true(primary,file)
+	pdfeature = convert_bedtools_to_panda(intersect)
+	pdcoord = label_expanded_coordinate_columns(pdfeature)
+	pdboundary = get_boundary_distances(pdcoord)
+	pdbinary = binary_boundary_distances(pdboundary) # may want to bin domain instead, and return which bin the elements are in
 
-
-# Question: How many UCEs are there in a domain
-
-# Question: Where in the domain are the UCEs
-
-# Question: What other features characterize domains with UCEs
-
-
-
-
-
-
-
-
-
-
-# map to features
-def coverage_primary_to_secondary(btfeature,queryfeature):
-	featurecov = queryfeature.coverage(btfeature)
-	return featurecov
-
-# get closest
-def primary_to_tertiary_distances(btfeature,queryfeature):
-	featuredis = btfeature.closest(queryfeature,d=True)
-	return featuredis
-
+# Query: What other features characterize domains with UCEs
+def additional_features(secondary,sclean,tfile):
+	tertiary = get_bedtools_features(tfile)
+	intersect = intersect_bedfiles_c_true(secondary,tertiary)
+	pdfeature = convert_bedtools_to_panda(intersect)
+	pdcoord = label_coordinate_columns(pdfeature)
+	pdcoord.columns.values[3]='intersect'
+	print sclean
+	print pdcoord.head()
 
 def main():
 	args = get_args()
 	
-	# 1) read in files from args
+	# read in files from args
 	primaryfile = args.file
 	secondaryfiles = [line.strip() for line in args.secondaryfeatures]
 	tertiaryfiles = [line.strip() for line in args.tertiaryfeatures]
@@ -120,31 +152,28 @@ def main():
 	allfiles = secondaryfiles + tertiaryfiles
 	allfiles.insert(0,primaryfile)
 	
-	print 'running script for {0}'.format(allfiles)
+	print 'running script with files: {0}'.format(allfiles)
 	
-	# 2) descriptive stats per feature file
+	# run descriptive stats per feature file
 	for file in allfiles:
-		btfeature = get_bedtools_features(file)
-		pdfeature = convert_bedtools_to_panda(btfeature)
-		pdcoord = label_coordinate_columns(pdfeature)
-		pdstats = descriptive_stats(pdcoord,'size')
+		pdstats = descriptive_stats(file)
 	
 	primary = get_bedtools_features(primaryfile)
 	
-	# 3) get overlaps - secondary
-	for file in secondaryfiles:
-		secondary = get_bedtools_features(file)
-		intersect = intersect_bedfiles_c_true(secondary,primary)
-		pdfeature = convert_bedtools_to_panda(intersect)
-		pdcoord = label_coordinate_columns(pdfeature)
-		pdcoord.columns.values[3]='intersect'
-		count_number_with_zero_overlaps(pdcoord,'intersect')
-		pdclean = remove_rows_with_no_overlaps(pdcoord,'intersect')
-		intersectstats = descriptive_stats(pdclean,'intersect')
-		print intersectstats
-		
-	
-	# 4) get nearest - tertiary
+	# process feature files
+	for sfile in secondaryfiles:
+		secondary,sclean = overlaping_features(primary,sfile)
+		print 'feature overlap stats on {0}'.format(sfile)
+		intersectstats = panda_describe_column(sclean,'intersect')
+# 		print intersectstats
+		locateing_features(primary,sfile)
+		for tfile in tertiaryfiles:
+			additional_features(secondary,sclean,tfile)
+
+
+
+
+
 
 
 
