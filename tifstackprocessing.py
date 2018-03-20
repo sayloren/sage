0f9ad048315c76d51d3b018da1187d6909e746ef
Wren <sayloren@gmail.com>
2018-03-20 17:54:50 -0400

    work on image processing tif script

--- tifstackprocessing.py
+++ tifstackprocessing.py
@@ -41,6 +41,9 @@ import pandas as pd
 import re
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage.feature import peak_local_max
+from skimage.morphology import watershed
+from scipy import ndimage
 # import tiffcapture as tc
 
 # set command line arguments
@@ -73,32 +76,77 @@ def parse_file_names(df):
 
 # read image
 def read_image(filename):
-	image = cv2.imread(filename)# 0 for grayscale
-	return image
+	return cv2.imread(filename)# 0 for grayscale
 
 # convert image to grey scale
 def convert_grayscale(image):
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	return gray
+	return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# adjust the contract of the image
-def histogram_equalization(image):
-	equalized = cv2.equalizeHist(image)
-	return equalized
+# lighten image with gamma correction
+def adjust_gamma(image,gamma):
+	#https://www.pyimagesearch.com/2015/10/05/opencv-gamma-correction/
+	# build a lookup table mapping the pixel values [0, 255] to their adjusted gamma values
+	invGamma = 1.0 / gamma
+	table = np.array([((i / 255.0) ** invGamma) * 255
+		for i in np.arange(0, 256)]).astype("uint8")
+	# apply gamma correction using the lookup table
+	return cv2.LUT(image,table)
 
-def find_contours(image):
-	im2, contours, hierarchy = cv2.findContours(image,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	drawing = cv2.drawContours(im2, contours, -1, (0,255,0), 3)
-	return drawing
+# blur to denoise
+def blur_signal(image,sigma):
+	#https://docs.opencv.org/3.0-beta/modules/imgproc/doc/filtering.html
+	return cv2.GaussianBlur(image,(5,5),10)
+
+# watershed for segmentation
+def watershed_segmentation_of_cells(image,thresh,gray):
+	#https://www.pyimagesearch.com/2015/11/02/watershed-opencv/
+	# compute the exact Euclidean distance from every binary pixel to the nearest zero pixel, then find peaks in this distance map
+	D = ndimage.distance_transform_edt(thresh)
+	localMax = peak_local_max(D, indices=False, min_distance=20,labels=thresh)
+	# perform a connected component analysis on the local peaks, using 8-connectivity, then appy the Watershed algorithm
+	markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
+	labels = watershed(-D, markers, mask=thresh)
+# 	print("[INFO] {} unique segments found".format(len(np.unique(labels)) - 1))
+	# loop over the unique labels returned by the Watershed algorithm
+	for label in np.unique(labels):
+		# if the label is zero, we are examining the 'background' so simply ignore it
+		if label == 0:
+			continue
+		# otherwise, allocate memory for the label region and draw it on the mask
+		mask = np.zeros(gray.shape, dtype="uint8")
+		mask[labels == label] = 255
+		# detect contours in the mask and grab the largest one
+		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)[-2]
+		c = max(cnts, key=cv2.contourArea)
+
+		# draw a circle enclosing the object
+		((x, y), r) = cv2.minEnclosingCircle(c)
+		cv2.circle(image, (int(x), int(y)), int(r), (0, 255, 0), 2)
+	return image
+
+
 
 # horizontally stack before and after images
 def stack_before_and_after_process_images(beforeimage,afterimage):
-	stacked = np.hstack((beforeimage,afterimage))
-	return stacked
+	return np.hstack((beforeimage,afterimage))
 
 # save image to png
 def save_image(image,filename):
 	cv2.imwrite('{0}.png'.format(filename),image)
+
+
+
+
+
+# adjust the contract of the image
+def histogram_equalization(image):
+	return cv2.equalizeHist(image)
+
+# find cell objects
+def find_contours(image):
+	im2, contours, hierarchy = cv2.findContours(image,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	return cv2.drawContours(im2, contours, -1, (0,255,0), 3)
 
 # order of operations to run histogram equalizing
 def run_equalize_image(filename):
@@ -122,18 +170,36 @@ def main():
 	groupsamples = pdfiles.groupby(['sample'])
 	
 	# iterate through groups
-	for name, group in groupsamples:
+	for name,group in groupsamples:
 	
 		# find the dapi channel/ channel where the nucleus is indicated
 		dapigroup = group[group['channel'] == dapchannel]
 		
 		# read in each filename
 		for filename in dapigroup['filename']:
-			run_equalize_image(filename)
-			# find contours
-			# convex hulls
-	
-	
+# 			run_equalize_image(filename)
+
+			# read in image
+			dapiimage = read_image(filename)
+			# change to grey instead of with the channel color
+			dapigray = convert_grayscale(dapiimage)
+			# gamma correction (camera sensitivity to eye sensitivity)
+			dapigamma = adjust_gamma(dapigray,10.0)
+			# gaussian blur filter to de-noise
+			dapiblur = blur_signal(dapigamma,3)
+			# equalize histogram
+			dapiequalized = histogram_equalization(dapiblur)
+			
+			
+			
+			
+			dapistacked = stack_before_and_after_process_images(dapigamma,dapiequalized)
+			save_image(dapistacked,'test')
+
+
+
+
+
 	# Equalized hist
 # 			img = cv2.imread(filename,0) # 0 for grayscale
 # 			equ = cv2.equalizeHist(img)
@@ -157,25 +223,17 @@ def main():
 # 			cv2.destroyWindow(name)
 
 	# Potential Helpful Websites
+	# http://mahotas.readthedocs.io/en/latest/
+	# https://docs.opencv.org/master/d9/df8/tutorial_root.html
+	# https://docs.opencv.org/master/de/d7a/tutorial_table_of_content_core.html
+	# https://docs.opencv.org/master/d7/da8/tutorial_table_of_content_imgproc.html
+	# https://www.mathworks.com/company/newsletters/articles/the-watershed-transform-strategies-for-image-segmentation.html?refresh=true
+	# http://imagej.net/Nuclei_Watershed_Separation
 	# https://docs.opencv.org/3.1.0/d5/daf/tutorial_py_histogram_equalization.html
 	# https://docs.opencv.org/2.4.13.4/doc/tutorials/imgproc/table_of_content_imgproc/table_of_content_imgproc.html
 	# https://mlbernauer.wordpress.com/2014/12/12/statistical-method-for-automated-cell-detection-and-counting/
 	# https://blogs.mathworks.com/steve/2006/06/02/cell-segmentation/
 	# https://stackoverflow.com/questions/7589012/combining-two-images-with-opencv
 
-	# Potential Steps
-	# 2) info 
-		# a) inputs args used; pixel conversion and z step size
-	# 3) imaging processing (contract, edges, hulls)
-		# a) background correction (tiny spots, large spots)
-		# b) smoothing
-		# c) segmentation
-	# 4) locate channel with nucleus, get some info on volume and area
-		# a) label and count nuclei, diameter, stats
-		# b) measure nuclear shape
-	# 5) within nucleus, locate signal in other channels
-		# a) foci diameter, foci count, foci distances, centroids, stats
-	# 6) print some pictures
-	
 if __name__ == "__main__":
 	main()
